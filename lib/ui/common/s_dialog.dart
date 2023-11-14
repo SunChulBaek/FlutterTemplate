f3021d87cd12ea70cb4cad9f9dc3775dafa520d2
x1210x <x1210x@gmail.com>
2023-11-14 14:03:04 +0900

    다이얼로그 쓰기 편하게

--- lib/ui/common/s_dialog.dart
+++ lib/ui/common/s_dialog.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-typedef SDialogButtonCallback = void Function();
+typedef SDialogButtonCallback<T> = void Function(T?);
 
-class SDialog extends StatelessWidget {
-  static const resultPositive = "positive";
-  static const resultNegative = "negative";
+class _SDialogData<T> {
+  _SDialogData({
+    required this.result,
+    this.data,
+  });
+  final String result;
+  final T? data;
+}
+
+class SDialog<T> extends StatefulWidget {
+  static const _resultPositive = "positive";
+  static const _resultNegative = "negative";
+  static const activeColorForChoice = Colors.cyan;
 
   const SDialog({
     this.title,
-    required this.content,
+    this.content,
+    this.choices,
+    this.init,
+    this.activeColor,
+    this.inactiveColor,
     this.positiveButton,
     this.negativeButton,
     this.onClickPositive,
@@ -17,44 +31,100 @@ class SDialog extends StatelessWidget {
   });
 
   final String? title;
-  final Widget content;
+  final Widget? content;
+
+  // choice
+  final List<String>? choices;
+  final int? init;
+  final Color? activeColor;
+  final Color? inactiveColor;
+
   final String? positiveButton;
   final String? negativeButton;
-  final SDialogButtonCallback? onClickPositive;
-  final SDialogButtonCallback? onClickNegative;
+  final SDialogButtonCallback<T>? onClickPositive;
+  final SDialogButtonCallback<T>? onClickNegative;
+
+  @override
+  State<StatefulWidget> createState() => _SDialogState();
+
+  void show(BuildContext context) => showDialog<_SDialogData<T>>(
+    context: context,
+    builder: (context) => this
+  ).then((value) {
+    if (value?.result == _resultPositive) {
+      onClickPositive?.call(value?.data);
+    } else if (value?.result == _resultNegative) {
+      onClickNegative?.call(value?.data);
+    }
+  });
+}
+
+class _SDialogState extends State<SDialog> {
+  int? _index;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _index = widget.init;
+    });
+  }
 
   @override
   Widget build(BuildContext context) => AlertDialog(
     backgroundColor: Colors.white,
     surfaceTintColor: Colors.transparent,
-    title: title != null ? Text(title!) : null,
-    content: content,
+    title: widget.title != null ? Text(widget.title!) : null,
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (widget.content != null)
+          widget.content!,
+        for (int i = 0; i < (widget.choices?.length ?? 0); i++)
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _index = i;
+              });
+            },
+            child: Text(
+              widget.choices![i],
+              style: TextStyle(
+                color: i == (_index ?? 0)
+                    ? (widget.activeColor ?? SDialog.activeColorForChoice)
+                    : widget.inactiveColor
+              )
+            )
+          )
+      ]
+    ),
     actions: [
-      if (positiveButton != null)
+      if (widget.positiveButton != null)
         TextButton(
-          child: Text(positiveButton!),
+          child: Text(widget.positiveButton!),
           onPressed: () {
-            Navigator.pop(context, resultPositive);
+            Navigator.pop(
+              context,
+              _SDialogData(
+                result: SDialog._resultPositive,
+                data: _index
+              )
+            );
           }
         ),
-      if (negativeButton != null)
+      if (widget.negativeButton != null)
         TextButton(
-          child: Text(negativeButton!),
+          child: Text(widget.negativeButton!),
           onPressed: () {
-            Navigator.pop(context, resultNegative);
+            Navigator.pop(
+              context,
+              _SDialogData(
+                result: SDialog._resultNegative,
+                data: _index
+              )
+            );
           }
         )
     ],
   );
-
-  void show(BuildContext context) => showDialog<String>(
-    context: context,
-    builder: (context) => this
-  ).then((value) {
-    if (value == resultPositive) {
-      onClickPositive?.call();
-    } else if (value == resultNegative) {
-      onClickNegative?.call();
-    }
-  });
 }
